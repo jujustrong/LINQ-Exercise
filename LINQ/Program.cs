5b3a90be230c4c329a452269c7855acddab3e6a8
Julian Armstrong <julian.armstrong8@gmail.com>
2024-09-03 11:58:07 -0600

    completed Exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Threading.Channels;
+using LINQ;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string> {"Dragons Dogma 2", "Red Dead Redemption 2", "Elden Ring", "Stardew Valley"};
+            var counter = 0;
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine($"{counter+=1}. {x}"));
+
+
+
         }
     }
 }
